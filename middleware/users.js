4e5b75bee8b3309ab847f9a66b54a25752ca02a3
eric matutu <ericmatutu125@gmail.com>
2025-05-01 23:34:59 +0300

    authentication: bug encountered fixing

---
+++ middleware/users.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+module.exports = {
+    validateRegister: (req, res, next) => {
+        //username min length < 3
+        if(!req.body.username || req.body.username.length < 3) {
+            return res.status(400).send({
+                message : 'Enter username with min. 3 chars'
+            });
+        }
+    
+        //password min 6 chars
+        if(!req.body.password || req.body.password.length < 6){
+            return res.status(400).send({
+                message : 'Password too short min. 6 chars'
+            });
+        }
+        // password(repeat) must match
+        if(!req.body.password_repeat || req.body.password !== req.body.password_repeat){
+            return res.status(400).send({
+                message : 'passwords dont match'
+            });
+        }
+        next();
+    }
+
+}
+
+// export default validateRegister;
--- routes/router.js
+++ routes/router.js
@@ -8,13 +8,48 @@ import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 
 import conn from '../database/db.js';
-// import userMiddleware from '../middleware/users.js';
+import userMiddleware from '../middleware/users.js';
 
 
 // 
-router.post('/sign-up', (req, res, next) =>{
-    res.send('hey there stranger..sign up');
-})
+router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) =>{
+    conn.query(
+        `SELECT user_id FROM users WHERE LOWER(username) = LOWER(?);`,
+        [req.body.username],
+        (err, result) =>{
+            if(result && result.length){
+                //error
+                return res.status(409).send({
+                    message : 'username aready in use!'
+                });
+            } else {
+                // username not in use
+                bcrypt.hash(req.body.password, 10, (err, hash)=>{
+                    if(err){
+                        return res.status(500).send({
+                            message : err,
+                        });
+                    } else {
+                        conn.query(
+                            'INSERT INTO users (user_id, username, password, email, created_at) VALUES (?,?,?,?, now());',
+                            [uuidv4(), req.body.username,hash, req.body.email,],
+                            (err,result)=>{
+                                if(err){
+                                    return res.status(400).send({
+                                        message : err,
+                                    });
+                                }
+                                return res.status(201).send({
+                                    message : 'Registered!',
+                                });
+                            }
+                        );
+                    }
+                });
+            }
+        }
+    );
+});
 
 router.post('/login', (req, res, next) => {
     res.send('wecome back.. sign in')
