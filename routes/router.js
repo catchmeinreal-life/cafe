518c412003b7e8a904bb2fc2304c7357d991687d
eric matutu <ericmatutu125@gmail.com>
2025-05-02 00:51:25 +0300

    Authentication: routes already set up

--- routes/router.js
+++ routes/router.js
@@ -52,8 +52,55 @@ router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) =>{
 });
 
 router.post('/login', (req, res, next) => {
-    res.send('wecome back.. sign in')
-})
+    conn.query(
+        'SELECT * FROM users WHERE username = ?;',
+        [req.body.username],
+        (err, result) =>{
+            if(err){
+                return res.status(400).send({
+                    message : err,
+                });
+            }
+            if(!result.length){
+                return res.status(400).send({
+                    message : 'Username or password incorrect',
+                });
+            }
+
+            bcrypt.compare(
+                req.body.password, result[0]['password'],
+                (bErr, bResult)=>{
+                    if(bErr){
+                        return res.status(400).send({
+                            message : 'Username or password incorrect!',
+                        });
+                    }
+                    if(bResult){
+                        //password match
+                        const token = jwt.sign(
+                            {
+                                username: result[0].username,
+                                userId: result[0].user_id,
+                            },
+                            'SECRETKEY',
+                            { expiresIn: '7d'}
+                        );
+                        conn.query('UPDATE users SET last_login = now() WHERE user_id = ?;',
+                        [result[0].user_id]);
+                        return res.status(200).send({
+                            message : 'Logged in!',
+                            token,
+                            user: result[0],
+                        });
+                    }
+                    return res.status(400).send({
+                        message : 'Username or password incorrect!',
+                    });
+                }
+            );
+        }
+    );
+});
 
 // authenticated pages
 router.get('/profile', (req, res)=>{
