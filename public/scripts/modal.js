a60cd20b314321db3c631a4d4d140f85cb250d21
eric matutu <ericmatutu125@gmail.com>
2025-05-02 13:49:08 +0300

    form ui: set up and navigable

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,5 +1,5 @@
-let logIn = document.getElementById('id01');
-let signIn= document.getElementById('id02');
+// let logIn = document.getElementById('id01');
+// let signIn= document.getElementById('id02');
 
 
 //when user clicks anywhere outside the modal, close it
@@ -9,12 +9,34 @@ window.onclick = function (event) {
     }
 }
 
-function showLoginForm(){
-    signIn.classList.remove('active-modal');
-    logIn.classList.add('active-modal' );
-    // alert('cicked');
+
+function showLoginForm() {
+    let element = document.getElementById("id02");
+    let element1 = document.getElementById("id01");
+    element.classList.remove("active-modal");
+    element1.classList.toggle("active-modal");
+
+
+    
 }
-function showSignupForm(){
-    logIn.classList.remove('active-modal');
-    signIn.classList.add('active-module' );
+
+function showSignUpForm() {
+    let element = document.getElementById("id02");
+    let element1 = document.getElementById("id01");
+    element1.classList.remove("active-modal");
+    element.classList.toggle("active-modal");
 }
+// var element = document.getElementById("myElement");
+// if (element.classList.contains("myClass")) {
+// element.classList.remove("myClass");
+// } else {
+// element.classList.add("myClass");
+// }
+
+
+// function showLoginForm(){
+//     signIn.classList.remove('active-modal');
+//     logIn.classList.add('active-modal' );
+//     // alert('cicked');
+// }
+
