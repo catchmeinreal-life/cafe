85c3ae316b58fd45884a022c3794050b3ca4bbec
eric matutu <ericmatutu125@gmail.com>
2025-05-02 03:46:15 +0300

    form ui: modification process

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,8 +1,20 @@
-let modal = document.getElementById('id01');
+let logIn = document.getElementById('id01');
+let signIn= document.getElementById('id02');
+
 
 //when user clicks anywhere outside the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function showLoginForm(){
+    signIn.classList.remove('active-modal');
+    logIn.classList.add('active-modal' );
+    // alert('cicked');
+}
+function showSignupForm(){
+    logIn.classList.remove('active-modal');
+    signIn.classList.add('active-module' );
 }
