e6938525446482bafc99469d907c86cbbcd24081
eric matutu <ericmatutu125@gmail.com>
2025-05-19 14:41:16 +0300

    landing page -home section layout - swiper functionality 3pages - next commit is adding css

---
+++ public/scripts/main/main.js
@@ -0,0 +1,17 @@
+// swiper for home section
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,  //false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
