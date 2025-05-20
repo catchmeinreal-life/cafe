7a0024d6f8605575a72f4c31bcb1d5ef721c28dc
eric matutu <ericmatutu125@gmail.com>
2025-05-20 13:07:03 +0300

    made it responsive- time for dynamic content

--- public/scripts/main/main.js
+++ public/scripts/main/main.js
@@ -15,3 +15,16 @@ var swiper = new Swiper(".home", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+let menu = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navBar.classList.remove('active');
+}
